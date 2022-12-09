98f7f02abd5f7b800a5df98d34b02a0abfc94a31
wallas <wagratom111@gmail.com>
2022-12-09 09:33:53 -0300

    ex00 ok

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: wwallas- <wwallas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 11:38:23 by wwallas-          #+#    #+#             */
-/*   Updated: 2022/12/09 08:46:15 by wwallas-         ###   ########.fr       */
+/*   Updated: 2022/12/09 09:32:58 by wwallas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,14 @@
 void	write_word(char	*word)
 {
 	for (int letter = 0; word[letter]; letter++)
-			std::cout << (char)toupper(word[letter]);
+			std::cout << (char)std::toupper(word[letter]);
 	std::cout << " ";
 }
 
 int main(int argc, char	*argv[])
 {
 	for (int i = 1; argv[i]; i++)
-	{
 		write_word(argv[i]);
-	}
+	std::cout << "\n";
 	return (0);
 }
