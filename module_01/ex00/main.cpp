5fd7920af328945d76fcec87ef1326abe744b4a7
wallas <wagratom111@gmail.com>
2022-12-14 17:40:21 -0300

    passando no pdf

--- module_01/ex00/main.cpp
+++ module_01/ex00/main.cpp
@@ -6,11 +6,14 @@
 /*   By: wwallas- <wwallas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 15:03:48 by wwallas-          #+#    #+#             */
-/*   Updated: 2022/12/14 15:09:27 by wwallas-         ###   ########.fr       */
+/*   Updated: 2022/12/14 17:39:43 by wwallas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <main.hpp>
+# include <Zombie.hpp>
+
+Zombie*		newZombie( std::string name );
+void		randomChump( std::string name );
 
 int main()
 {
@@ -18,7 +21,6 @@ int main()
 
 	ptr = newZombie( "programmer" );
 	ptr->announce();
-	delete	(ptr);
-
 	randomChump( "Sauro" );
+	delete	(ptr);
 }
