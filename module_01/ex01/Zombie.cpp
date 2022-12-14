a2f9a607a8c61d5dbe4adad17372fefbfb8520a4
wallas <wagratom111@gmail.com>
2022-12-14 15:52:15 -0300

    pronto e refatorado

--- module_01/ex01/Zombie.cpp
+++ module_01/ex01/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: wwallas- <wwallas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 11:36:34 by wwallas-          #+#    #+#             */
-/*   Updated: 2022/12/14 15:46:34 by wwallas-         ###   ########.fr       */
+/*   Updated: 2022/12/14 15:51:59 by wwallas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,13 @@
 
 Zombie::Zombie( std::string name )
 {
-	std::cout << "called Zombie ";
-	std::cout << name << std::endl;
+	this->name = name;
+	std::cout << "called Zombie " << name << std::endl;
 }
 
 Zombie::~Zombie( void )
 {
-	std::cout << "destroyed Zombie ";
-	std::cout << name << std::endl;
+	std::cout << "destroyed Zombie " << name << std::endl;
 }
 
 void	Zombie::set_name( std::string name )
@@ -42,15 +41,11 @@ void	Zombie::announce( void )
 
 Zombie*		newZombie( std::string name )
 {
-	Zombie	*ptr = new Zombie( name );
-
-	ptr->set_name(name);
-	return (ptr);
+	return new Zombie( name );
 }
 
 void randomChump( std::string name )
 {
 	Zombie	new_zombie(name);
-	new_zombie.set_name(name);
 	new_zombie.announce();
 }
